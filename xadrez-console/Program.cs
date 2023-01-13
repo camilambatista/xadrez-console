6bafa54cfc20d3f11bc666910e2edbe785c19a64
Camila Batista <camissk@hotmail.com>
2023-01-13 07:46:41 -0300

    Comecando a implementar o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -6,10 +6,7 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao p;
-
-            p = new Posicao(3, 4);
-            Console.WriteLine($"Posição: {p}");
+            Tabuleiro.Tabuleiro tab = new Tabuleiro.Tabuleiro(8, 8);
         }
     }
 }
---
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿using Tabuleiro.Enums;
+
+namespace Tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QuantidadeMovimentos { get; protected set; }
+        public Tabuleiro Tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Tabuleiro tabuleiro, Cor cor)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            QuantidadeMovimentos = 0;
+            Tabuleiro = tabuleiro;
+        }   
+    }
+}
