f35a7b64106b72e0f381a6cd3ab4c957a46ff3ff
Camila Batista <camissk@hotmail.com>
2023-01-23 07:33:06 -0300

    Implementando a logica de xequemate

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -42,6 +42,9 @@ namespace xadrez_console
                         Console.ReadLine();
                     }
                 }
+
+                Console.Clear();
+                Tela.ImprimirPartida(partida);
             }
             catch (TabuleiroException e)
             {
